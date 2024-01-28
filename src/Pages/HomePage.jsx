c8fdd062aeb9f314d8792779ccb575ad6989fe2f
Hrishita-Paul <hrishita.paul03@gmail.com>
2024-01-28 17:30:14 +0530

    fix:Responsiveness

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -8,8 +8,8 @@ export default function HomePage() {
         <div className="hero-container">
         <div class="row">
     <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-      <h1>Better Solutions For The Waste Around You!</h1>
-      <h2>Keep the Environment Healthy</h2>
+      <h1 className="title">Better Solutions For The Waste Around You!</h1>
+      <h2 className="subtitle">Keep the Environment Healthy</h2>
   
       <div class="d-flex justify-content-center justify-content-lg-start">
         <button class="btn btn-primary"><a href="/user" className="scroll">Get Started</a></button>
